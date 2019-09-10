24ec8aa3174ed15dffcc6dd4189929f74842506f
MajaBrog <majaa.brog@gmail.com>
2019-09-10 21:24:17 +0200

    New class User and Calculator added and tested

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calc= new Calculator();
+
+        Integer addTest=calc.add(2,3);
+
+        if (addTest.equals(2+3)){
+            System.out.println("Add method OK");
+        } else {
+            System.out.println("Add method not OK");
+        }
+
+        Integer subtractTest=calc.subtract(5,3);
+
+        if (subtractTest.equals(5-3)){
+            System.out.println("Subtract method OK");
+        } else {
+            System.out.println("Subtract method not OK");
+        }
     }
 }
