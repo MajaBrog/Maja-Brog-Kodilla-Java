6344374c66c8a5c11b0beb14a35aaf46af2127d2
MajaBrog <majaa.brog@gmail.com>
2019-09-22 11:58:57 +0200

    add 7.5

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,16 @@
+package com.kodilla.stream.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArrayOperationsTestSuite {
+    @Test
+    public void tstGetAverage(){
+        //Given
+        int numbers [] = {1,2,3,4,5,6,7,8,9,10};
+        //When
+        double average = ArrayOperations.getAverage(numbers);
+        //Then
+        Assert.assertEquals(5.5, average, 0.0001);
+    }
+}
